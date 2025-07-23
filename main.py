0978fbb20d38e2f060cc3fb92803cbb92752ee26
Yadh <yadhnikawakde@gmail.com>
2025-07-23 23:22:19 +0530

    fixed port

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def update_status():
     return jsonify({"msg": "Status updated ✅", "new_status": data})
 
 if __name__ == "__main__":
-    app.run(debug = True, port = "0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
